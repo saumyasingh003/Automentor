710388e654e25df7987b4929e22d6075c1e8f32c
saumyasingh003 <saumyasingh98982@gmail.com>
2025-09-11 00:22:41 +0530

    updates

--- src/modules/meetings/components/meeting-form.tsx
+++ src/modules/meetings/components/meeting-form.tsx
@@ -149,7 +149,7 @@ export const MeetingForm = ({
                   />
                 </FormControl>
                 <FormDescription>
-                  Not found what you're looking for?{" "}
+                  Not found what you&apos;re looking for?{" "}
                   <button
                     type="button"
                     className="text-green-700 hover:underline"
